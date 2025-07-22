bcc41ac96ec5290324b57232bc9d3f3cf1f20b46
unknown <sago980302@hotmail.com>
2025-07-21 20:53:44 -0500

    ajuste ruta del template pdf

--- netlify/functions/get-pdf.js
+++ netlify/functions/get-pdf.js
@@ -2,16 +2,23 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async () => {
-  const filePath = path.resolve(__dirname, '../../assets/templatev2.pdf');
-  const file = fs.readFileSync(filePath);
+  try {
+    const filePath = path.resolve(__dirname, 'templatev2.pdf');
+    const file = fs.readFileSync(filePath);
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/pdf',
-      'Access-Control-Allow-Origin': '*',
-    },
-    body: file.toString('base64'),
-    isBase64Encoded: true,
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/pdf',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: file.toString('base64'),
+      isBase64Encoded: true,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `Error: ${err.message}`,
+    };
+  }
 };
