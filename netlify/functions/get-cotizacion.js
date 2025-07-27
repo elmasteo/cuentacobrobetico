9adea837455936ca57fffa716f065fc1a5e2730d
unknown <sago980302@hotmail.com>
2025-07-27 13:25:38 -0500

    agregar cotizaciones

---
+++ netlify/functions/get-cotizacion.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async () => {
+  try {
+    const filePath = path.resolve(__dirname, "cotizacion-template.pdf");
+    const fileBuffer = fs.readFileSync(filePath);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/pdf",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: fileBuffer.toString("base64"),
+      isBase64Encoded: true, // 👈 clave
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `Error: ${err.message}`,
+    };
+  }
+};
